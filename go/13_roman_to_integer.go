7bc4bc6dbe93fc39d768be4492775bde49fa3d1e
林鹏飞 <lynn_lin@intsig.net>
2019-09-24 20:47:56 +0800

    /*

--- go/13_roman_to_integer.go
+++ go/13_roman_to_integer.go
@@ -2,35 +2,22 @@
  * 遍历字符串，使用switch将罗马数转成十进制整数，记得处理三个特殊case I,X,C
  */
 func romanToInt(s string) int {
-    var result int = 0;
+    var result int = 0
+    var rom2int = map[byte]int {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
     for i := 0; i < len(s); i++ {
-        switch s[i] {
-        case 'I':
-            if (i < len(s)-1) && (s[i+1] == 'V' || s[i+1] == 'X') {
-                result -= 1
-            } else {
-                result += 1
-            }
-        case 'V':
-            result += 5
-        case 'X':
-            if (i < len(s)-1) && (s[i+1] == 'L' || s[i+1] == 'C') {
-                result -= 10
-            } else {
-                result += 10
-            }
-        case 'L':
-            result += 50
-        case 'C':
-            if (i < len(s)-1) && (s[i+1] == 'D' || s[i+1] == 'M') {
-                result -= 100
-            } else {
-                result += 100
-            }
-        case 'D':
-            result += 500
-        case 'M':
-            result += 1000
+        if i < len(s)-1 && rom2int[s[i]] < rom2int[s[i+1]] {
+            result -= rom2int[s[i]]
+        } else {
+            result += rom2int[s[i]]
         }
     }
     return result
